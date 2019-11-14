c2aa1672f08a02e034edef767f9096e15fd1c4c7
ilya <ilya@ilyas-MacBook-Air.local>
2019-11-14 22:40:48 +0300

    Pool.sol set() test

--- test/Pool.ts
+++ test/Pool.ts
@@ -1,3 +1,5 @@
+import Web3Utils from 'web3-utils';
+
 import { PoolContract, PoolInstance} from "../types/truffle-contracts/index";
 // tslint:disable-next-line:no-var-requires
 const { BN, constants, expectEvent, shouldFail } = require("@openzeppelin/test-helpers");
@@ -25,7 +27,30 @@ contract("Pool", async ([_, owner,  wallet1, wallet2, wallet3, wallet4, wallet5]
         (await pool.name()).should.equal("creditPool");
     });
 
-    it("should have  proper description", async () => {
+    it("should have proper description", async () => {
         (await pool.description()).should.equal("Great Pool");
+    });
+
+    it("should set new module for given name", async () => {
+        const input = {
+            _name: 'Node1',
+            _module: wallet5,
+            _constant: true,
+        };
+
+        await pool.set(
+            input._name,
+            input._module,
+            input._constant,
+            { 
+                from: owner,
+            }
+        );
+
+        (
+            await pool.isConstant(input._name) && 
+            await pool.get(input._name) === wallet5
+        )
+        .should.equal(true);
     });
 });
